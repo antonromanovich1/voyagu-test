918595bff90d8e8d015457800ae22200bf05d5e6
anton.ramanovich <a.ramanovich@andersenlab.com>
2025-03-07 21:47:08 +0100

    adds theme preset

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,22 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import Aura from '@primeng/themes/aura';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 
 import { routes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { VoyaguTest } from './utils/voyagu-theme.presets';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes),provideAnimationsAsync(),
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    provideHttpClient(),
     providePrimeNG({
-        theme: {
-            preset: Aura
-        }
-    })]
+      theme: {
+        preset: VoyaguTest,
+      },
+    }),
+  ],
 };
-
---
+++ src/app/utils/voyagu-theme.presets.ts
@@ -0,0 +1,80 @@
+import Aura from '@primeng/themes/aura';
+import { definePreset } from '@primeng/themes';
+
+export const VoyaguTest = definePreset(Aura, {
+  semantic: {
+    primary: {
+      50: '#f5f8fe',
+      100: '#cddffa',
+      200: '#a6c6f6',
+      300: '#7eadf2',
+      400: '#5794ee',
+      500: '#2f7bea',
+      600: '#2869c7',
+      700: '#2156a4',
+      800: '#1a4481',
+      900: '#13315e',
+      950: '#0c1f3b',
+    },
+    formField: {
+      paddingX: '0.75rem',
+      paddingY: '0.5rem',
+      sm: {
+        fontSize: '0.875rem',
+        paddingX: '0.625rem',
+        paddingY: '0.375rem',
+      },
+      lg: {
+        fontSize: '1.125rem',
+        paddingX: '0.875rem',
+        paddingY: '0.625rem',
+      },
+      borderRadius: '{border.radius.md}',
+      focusRing: {
+        width: '0',
+        style: 'none',
+        color: 'transparent',
+        offset: '0',
+        shadow: 'none',
+      },
+      transitionDuration: '{transition.duration}',
+    },
+    list: {
+      padding: '0.25rem 0.25rem',
+      gap: '2px',
+      header: {
+        padding: '0.5rem 1rem 0.25rem 1rem',
+      },
+      option: {
+        padding: '0.5rem 0.75rem',
+        borderRadius: '{border.radius.sm}',
+      },
+      optionGroup: {
+        padding: '0.5rem 0.75rem',
+        fontWeight: '600',
+      },
+    },
+    content: {
+      borderRadius: '{border.radius.md}',
+    },
+    overlay: {
+      select: {
+        borderRadius: '{border.radius.md}',
+        shadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1)',
+      },
+      popover: {
+        borderRadius: '{border.radius.md}',
+        padding: '0.75rem',
+        shadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1)',
+      },
+      modal: {
+        borderRadius: '{border.radius.xl}',
+        padding: '1.25rem',
+        shadow: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 8px 10px -6px rgba(0, 0, 0, 0.1)',
+      },
+      navigation: {
+        shadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -2px rgba(0, 0, 0, 0.1)',
+      },
+    },
+  },
+});
