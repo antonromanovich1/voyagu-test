415c8be37bb3c202344a985c261c64bb36fc150b
anton.ramanovich <a.ramanovich@andersenlab.com>
2025-03-07 20:23:58 +0100

    basic routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { FlightsComponent } from './components/flights/flights.component';
+import { BookFormComponent } from './components/book-form/book-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'flights',
+    component: FlightsComponent,
+  },
+  {
+    path: 'booking',
+    component: BookFormComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/flights',
+    pathMatch: 'full',
+  },
+];
