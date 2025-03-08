983e36cd1ffe675c37aba5874287bf6334a0538f
anton.ramanovich <a.ramanovich@andersenlab.com>
2025-03-08 14:18:48 +0100

    form init

--- src/app/components/flights/flights.component.ts
+++ src/app/components/flights/flights.component.ts
@@ -1,24 +1,35 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { SelectModule } from 'primeng/select';
 import { SliderModule } from 'primeng/slider';
 import { CheckboxModule } from 'primeng/checkbox';
 import { FlightService } from '../../services/flight.service';
-import { FormsModule } from '@angular/forms';
+import { FormArray, FormBuilder, FormsModule, NonNullableFormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
 import { ButtonModule } from 'primeng/button';
+import { filter, map } from 'rxjs';
+import { AsyncPipe, CurrencyPipe } from '@angular/common';
+import { FilterKeysService } from '../../services/filter-keys.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-flights',
-  imports: [SelectModule, SliderModule, FormsModule, CheckboxModule, FlightCardComponent, ButtonModule],
+  imports: [
+    SelectModule,
+    SliderModule,
+    FormsModule,
+    CheckboxModule,
+    ButtonModule,
+    AsyncPipe,
+    FlightCardComponent,
+    ReactiveFormsModule,
+    CurrencyPipe,
+  ],
   templateUrl: './flights.component.html',
   styleUrl: './flights.component.scss',
 })
 export class FlightsComponent implements OnInit {
-  ngOnInit(): void {
-    this.flightService.getFlights().subscribe((flights) => console.log(flights));
-  }
+  ITEMS_PER_PAGE = 5;
 
-  private flightService = inject(FlightService);
   protected readonly sortOptions = [
     { label: 'Price (Lowest)', value: 'price-low' },
     { label: 'Price (Highest)', value: 'price-high' },
@@ -27,12 +38,56 @@ export class FlightsComponent implements OnInit {
     { label: 'Duration  (Short)', value: 'duration' },
   ];
 
-  protected readonly stopOptions = [
-    { label: 'All stops', value: 'all-stop' },
-    { label: 'Nonstop', value: 'non-stop' },
-    { label: '1 stop', value: 'one-stop' },
-    { label: '2 stops', value: 'two-stop' },
-  ];
+  private flightService = inject(FlightService);
+  protected filterKeysService = inject(FilterKeysService);
+  private fb = inject(NonNullableFormBuilder);
+  private destroyRef$ = inject(DestroyRef);
 
-  rangeValues: number[] = [0, 100];
+  filtersData$ = this.filterKeysService.filtersData$;
+
+  readonly form = this.fb.group({
+    sort: [this.sortOptions[0].value],
+    range: [[0, 0]],
+    allStop: [true],
+    stops: this.fb.array([]),
+  });
+
+  get stops(): FormArray {
+    return this.form.get('stops') as FormArray;
+  }
+
+  stopTest: string[] = [];
+
+  protected minRange = 500;
+  protected maxRange = 6000;
+
+  ngOnInit(): void {
+    this.flightService.getFlights();
+    this.filtersData$
+      .pipe(
+        takeUntilDestroyed(this.destroyRef$),
+        filter((f) => !!f.priceHigh),
+      )
+      .subscribe((filters) => {
+        if (filters) {
+          this.minRange = Math.floor(filters.priceLow / 100) * 100;
+          this.maxRange = Math.ceil(filters.priceHigh / 100) * 100;
+
+          this.form.get('range')?.setValue([filters.priceLow, filters.priceHigh]);
+          filters.stops.forEach((stop) => {
+            this.stops.push(this.fb.control(false));
+            this.stopTest.push(stop.toString());
+          });
+        }
+      });
+  }
+
+  test() {
+    console.log(this.form.value);
+  }
+
+  flightsToShow$ = this.flightService.flights$.pipe(map((flights) => flights.slice(0, this.ITEMS_PER_PAGE)));
+  // skip$ = new BehaviorSubject(this.ITEMS_PER_PAGE);
+  // sort$ = new BehaviorSubject('price-low');
+  // range$ = new BehaviorSubject(this.rangeValues);
 }
